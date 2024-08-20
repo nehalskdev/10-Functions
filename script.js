d1f9cc19b59b0cebc8cbb82c98ec9ac463f9b954
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-08-20 19:10:54 +0530

    starting second lec

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 'use strict';
 
-const bookings = [];
-const createBooking = function (flightNum, numPassenger = 1, price = 199) {
-   // Es-5 way of creating default parameters //
-    // numPassenger = numPassenger || 1;
-    // price = price || 199;
+// -------- default parameters --------------------------------
 
-    const booking = {
-      flightNum,
-      numPassenger,
-      price,
-    };
-    console.log(booking);
-    bookings.push(booking);
-}
+// const bookings = [];
+// const createBooking = function (flightNum, numPassenger = 1, price = 199) {
+//   // Es-5 way of creating default parameters //
+//   // numPassenger = numPassenger || 1;
+//   // price = price || 199;
 
-createBooking('LH123');
-createBooking('H123', 2, 800);
-createBooking('H123', undefined, 500); // if you dont want to specify the paramter argument and want it to take as per default parameters then just set it to 'undefined'
+//   const booking = {
+//     flightNum,
+//     numPassenger,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('H123', 2, 800);
+// createBooking('H123', undefined, 500); // if you dont want to specify the paramter argument and want it to take as per default parameters then just set it to 'undefined'
+ 
+------ // passing argument works ---
+
