b43b2d5fe80b605ac5bbd5870e4f105a899fb897
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-08-20 18:56:54 +0530

    ES5 way to add default parameter

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const bookings = [];
 const createBooking = function (flightNum, numPassenger, price) {
+    numPassenger = numPassenger || 1;
+    price = price || 199;
     const booking = {
       flightNum,
       numPassenger,
