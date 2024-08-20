255b5f71428d0610fb14c211e930feb5c9555d9b
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-08-20 18:58:28 +0530

    added Es-6 approach to add parameter

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const bookings = [];
-const createBooking = function (flightNum, numPassenger, price) {
-    numPassenger = numPassenger || 1;
-    price = price || 199;
+const createBooking = function (flightNum, numPassenger = 1, price = 199) {
+   // Es-5 way of creating default parameters //
+    // numPassenger = numPassenger || 1;
+    // price = price || 199;
+
     const booking = {
       flightNum,
       numPassenger,
